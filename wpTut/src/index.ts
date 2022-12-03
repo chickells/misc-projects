3318cc8090474a68d14f2e5920f0fbe21a37228f
Chase <cnickellsumn@gmail.com>
2022-12-03 05:35:25 -0800

    got css loader working, didn't link bundle.js in html index

--- wpTut/src/index.ts
+++ wpTut/src/index.ts
@@ -1,11 +1,13 @@
 import * as _ from 'lodash';
 import myName from './myName';
+import './style.css' 
 
 const component = () => {
     const element = document.createElement('div');
   
     // Lodash, now imported
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
     
   
     return element;
@@ -14,6 +16,7 @@ const component = () => {
 const componentTwo = () => {
   const element = document.createElement('div');
   element.innerHTML = _.join([`Sup lodash + ${myName('Chase')}`])
+  element.classList.add('hey');
 
   return element
 }
--- wpTut/webpack.config.js
+++ wpTut/webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-    ],
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
