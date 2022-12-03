5dedf2afe7a75881ce79735b22b3522e553faec6
Chase <cnickellsumn@gmail.com>
2022-12-03 06:08:34 -0800

    logo imported tried to wrap both createDiv functions into one big one but couldn't get it to call itself, said can't pass component() with 'void' as parameter....

--- wpTut/src/index.ts
+++ wpTut/src/index.ts
@@ -1,25 +1,32 @@
 import * as _ from 'lodash';
 import myName from './myName';
 import './style.css' 
+import Logo from './logo.svg'
 
-const component = () => {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-    
-  
-    return element;
-  }
+const firstDiv = () => {
+  const element = document.createElement('div');
 
-const componentTwo = () => {
+  // Lodash, now imported
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');    
+
+  return element;
+}
+
+const secondDiv = () => {
   const element = document.createElement('div');
   element.innerHTML = _.join([`Sup lodash + ${myName('Chase')}`])
   element.classList.add('hey');
 
+  const tehLogo = new Image()
+  tehLogo.src = Logo
+
+  element.appendChild(tehLogo)
+
   return element
 }
 
-document.body.appendChild(component());
-document.body.appendChild(componentTwo());
+  
+
+document.body.appendChild(firstDiv());
+document.body.appendChild(secondDiv());
