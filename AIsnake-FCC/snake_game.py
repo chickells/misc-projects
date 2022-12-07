137297cffbd4235353c07287f017ca2e572987f8
Chase <cnickellsumn@gmail.com>
2022-12-07 09:45:47 -0800

    removed last "game end" user input

--- AIsnake-FCC/snake_game.py
+++ AIsnake-FCC/snake_game.py
@@ -156,19 +156,3 @@ class SnakeGameAI:
             y -= BLOCK_SIZE
             
         self.head = Point(x, y)
-            
-
-if __name__ == '__main__':
-    game = SnakeGame()
-    
-    # game loop
-    while True:
-        game_over, score = game.play_step()
-        
-        if game_over == True:
-            break
-        
-    print('Final Score', score)
-        
-        
-    pygame.quit()
