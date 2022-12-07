669c00147f975cb23b5b040ef6691b7fba55bbbe
Chase <cnickellsumn@gmail.com>
2022-12-07 09:43:18 -0800

    updated movements based On which direction already moving

--- AIsnake-FCC/snake_game.py
+++ AIsnake-FCC/snake_game.py
@@ -133,7 +133,16 @@ class SnakeGameAI:
         clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
         idx = clock_wise.index(self.direction)
 
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clock_wise[idx] # no change
+        elif np.array_equal(action, [0, 1, 0]):
+            next_idx = (idx + 1) % 4
+            new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        else: # [0, 0, 1]
+            next_idx = (idx - 1) % 4
+            new_dir = clock_wise[next_idx] # left turn r -> u -> l -> d
 
+        self.direction = new_dir
 
         x = self.head.x
         y = self.head.y
