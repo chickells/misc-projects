5b18e3022c9444aebc9e0db5c01353d633aa6c82
Chase <cnickellsumn@gmail.com>
2022-12-07 09:25:52 -0800

    updating py game to be used by AI

--- AIsnake-FCC/snake_game.py
+++ AIsnake-FCC/snake_game.py
@@ -7,7 +7,12 @@ pygame.init()
 font = pygame.font.Font('arial.ttf', 25)
 #font = pygame.font.SysFont('arial', 25)
 
-
+# TO DO:
+# reset
+# reward
+# play(action) -> direction
+# game_iteration
+# is_collision
 
 class Direction(Enum):
     RIGHT = 1
@@ -27,7 +32,7 @@ BLACK = (0,0,0)
 BLOCK_SIZE = 20
 SPEED = 20
 
-class SnakeGame:
+class SnakeGameAI:
     
     def __init__(self, w=640, h=480):
         self.w = w
@@ -36,7 +41,11 @@ class SnakeGame:
         self.display = pygame.display.set_mode((self.w, self.h))
         pygame.display.set_caption('Snake')
         self.clock = pygame.time.Clock()
+        self.reset()
         
+        
+
+    def reset(self):
         # init game state
         self.direction = Direction.RIGHT
         
