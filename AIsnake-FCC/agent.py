b3fb88dc2a9e385dbb1ce43d87ce80443a01c1f2
Chase <cnickellsumn@gmail.com>
2022-12-07 22:38:18 -0800

    999999999999999999 and 98%

--- AIsnake-FCC/agent.py
+++ AIsnake-FCC/agent.py
@@ -135,7 +135,7 @@ def train():
 
             if score > record:
                 record = score
-                if score > (record * 0.9):
+                if score > (record * 0.98):
                     agent.model.save()
 
             print('Game: ', agent.n_games, 'Score: ', score, 'Record: ', record)
